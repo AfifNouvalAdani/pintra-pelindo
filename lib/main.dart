40d4f3e7e5d6b0208e5537c6edfe775a62d2871b
cleoalya <cleoalyakh@gmail.com>
2026-01-13 09:03:06 +0700

    new changes collaborator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'features/splash/splash_page.dart';
 import 'features/auth/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PintraApp());
 }
 
